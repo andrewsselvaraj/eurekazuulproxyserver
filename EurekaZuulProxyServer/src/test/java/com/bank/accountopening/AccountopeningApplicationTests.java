e86b594cf30777938da2bea5ef16e8e8837d4076
shiju <shiju@LAPTOP-QFQM3362>
2020-08-17 23:16:51 +0530

    Commit

---
+++ EurekaZuulProxyServer/src/test/java/com/bank/accountopening/AccountopeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.accountopening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountopeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
